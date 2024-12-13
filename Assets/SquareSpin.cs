43ac2e079f6228910bb644caad272d2c753595aa
Henrique Vinagre <68971545+henriquevinagre@users.noreply.github.com>
2024-12-13 22:53:03 +0000

    Square movement

--- Assets/SquareSpin.cs
+++ Assets/SquareSpin.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class SquareSpin : MonoBehaviour
 {
-    public GameObject square;
+    public float moveSpeed = 5f; // Movement speed of the player
+
+    private Vector2 movement; // Stores the input direction
+    public Rigidbody2D rb;   // Reference to the Rigidbody2D component
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        square.transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+        rb.transform.Rotate(new Vector3(0, 0, 180) * Time.deltaTime);
+
+        // Get input for horizontal and vertical axes
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Combine into a Vector2 for movement
+        movement = new Vector2(horizontal, vertical).normalized;
+    }
+
+    void FixedUpdate() {
+        // Move the player in the direction of the input
+        rb.linearVelocity = movement * moveSpeed;
     }
 }
