313f7bfa2120e30633dca37ba75c87daeb1e6d6c
Rodrigo Fialho <rodrigofialho.dev@gmail.com>
2024-12-15 19:06:33 +0000

    Enemy Animations

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,24 +9,53 @@ public class EnemyController : MonoBehaviour
 
     private GameObject player;
     private Rigidbody2D rb;
+    private Animator myAnimator;
 
-    private void Start() {
+    private Vector2 movementInput;
+    private Vector2 lastMoveDirection;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start() 
+    {
         player = GameObject.FindGameObjectWithTag("Player");
         if(player == null) {
             Debug.LogError("Player not found! Ensure the Player GameObject has the 'Player' tag.");
         }
-
-        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
         if(player == null)
             return;
 
-        // Calculate direction toward the player
-        Vector2 direction = (player.transform.position - transform.position).normalized;
+        //Store last move direction when we stop moving
+        float moveX = (player.transform.position - transform.position).normalized.x;
+        float moveY = (player.transform.position - transform.position).normalized.y;
 
-        rb.velocity = direction * speed;
+        if ((moveX == 0 && moveY == 0) && (movementInput.x != 0 || movementInput.y != 0))
+        {
+            lastMoveDirection = movementInput;
+        }
+
+        // Calculate direction toward the player
+        movementInput = (player.transform.position - transform.position).normalized;
+
+        Animations();
+
+        rb.velocity = movementInput * speed;
+    }
+
+    private void Animations()
+    {
+        myAnimator.SetFloat("MoveY", movementInput.y);
+        myAnimator.SetFloat("MoveX", movementInput.x);
+        myAnimator.SetFloat("MoveMagnitude", movementInput.magnitude);
+        myAnimator.SetFloat("LastMoveY", lastMoveDirection.y);
+        myAnimator.SetFloat("LastMoveX", lastMoveDirection.x);
     }
 
     public void Die() {
